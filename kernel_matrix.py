8dfb42a0e711b46ac71df67f6802681a2ce36cd4
suchana <suchanachakraborty@gmail.com>
2020-06-18 19:35:46 +0530

    nlp and graph

--- kernel_matrix.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from sklearn import metrics
-data  = pd.read_csv('abc.csv')
-
-quitmatrix = np.matrix([vectorfun.generate_vector(v) for k,v in data.column_name.iterkv()])
-dmat = metrics.pairwise_distances(quitmatrix, metric = 'cosine')
-
-
--- preprocessing.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def preprocess(self, element):
-    return re.sub('[a-zA-Z]', '', elemeny.lower()).ljust(3)
-
-#this function strip all the non alphanumeric characters, then transform all into lowercase characters and then remove all the extraneous white spacing and data
-
-
-preprocess()
